6a0c6b68073b01c5c9a850ad1afc2230f79c1b9b
Daniel <deltasierra777@gmail.com>
2024-05-03 14:23:36 -0500

    v1.0

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -1,51 +1,155 @@
-﻿using Dalamud.Game.Command;
+using Dalamud.Game.Command;
 using Dalamud.IoC;
 using Dalamud.Plugin;
-using System.IO;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
 using SamplePlugin.Windows;
-
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
+using Dalamud.Game.Text;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using System.Collections.Generic;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using Dalamud.Game.Gui.ContextMenu;
+using System;
+using System.Linq;
+using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.Interface.ImGuiNotification;
+using Dalamud.Interface.ImGuiNotification.EventArgs;
+using Dalamud.Game.Addon.Lifecycle;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using System.Runtime.InteropServices;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using ClickLib.Clicks;
+using System.Threading;
 namespace SamplePlugin;
 
 public sealed class Plugin : IDalamudPlugin
 {
-    private const string CommandName = "/pmycommand";
+    private const string CommandName = "/junk";
 
     private DalamudPluginInterface PluginInterface { get; init; }
     private ICommandManager CommandManager { get; init; }
+    private IChatGui Chat { get; init; }
+    private IContextMenu ContextMenu { get; init; }
+    private IGameGui GameUI { get; init; }
     public Configuration Configuration { get; init; }
 
     public readonly WindowSystem WindowSystem = new("SamplePlugin");
     private ConfigWindow ConfigWindow { get; init; }
     private MainWindow MainWindow { get; init; }
+    private List<InventoryItem> Junkpile { get; init; }
+    private List<string> JunkItemNames { get; init; }
+    private IAddonLifecycle AddonLifecycle {  get; init; }
+    private bool isDiscarding {  get; set; }
+    private SemaphoreSlim signal;
+
+    private static readonly int[] InventoryContainerArray = [0, 1, 2, 3];
+
+    private unsafe AgentInventoryContext* agentInventoryContext;
+    private unsafe InventoryManager* inventoryManager;
+
+    private unsafe void SetContexts()
+    {
+        agentInventoryContext = AgentInventoryContext.Instance();
+        inventoryManager = InventoryManager.Instance();
+    }
+
+    private unsafe void SelectYesNoSetup(AddonEvent type, AddonArgs args)
+    {
+        var addon = (AtkUnitBase*)args.Addon;
+        SelectYes((nint)addon);
+    }
+
+    private unsafe void SelectYes(IntPtr addon)
+    {
+        var dataPtr = (AddonSelectYesNoOnSetupData*)addon;
+        var addonPtr = (AddonSelectYesno*)addon;
+        var yesButton = addonPtr->YesButton;
+        if (yesButton != null && !yesButton->IsEnabled)
+        {
+            var flagsPtr = (ushort*)&yesButton->AtkComponentBase.OwnerNode->AtkResNode.NodeFlags;
+            *flagsPtr ^= 1 << 5;
+        }
+        if(isDiscarding) ClickSelectYesNo.Using(addon).Yes();
+    }
+
+    public void UpdateDiscardStatus(AddonEvent type, AddonArgs args)
+    {
+        signal.Release();
+        Chat.Print("release");
+    }
 
     public Plugin(
         [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
         [RequiredVersion("1.0")] ICommandManager commandManager,
-        [RequiredVersion("1.0")] ITextureProvider textureProvider)
+        [RequiredVersion("1.0")] ITextureProvider textureProvider, IChatGui chat, IGameGui gui, IContextMenu contextMenu, IAddonLifecycle addonLc)
     {
         PluginInterface = pluginInterface;
         CommandManager = commandManager;
+        Chat = chat;
+        GameUI = gui;
+        Junkpile = new List<InventoryItem>();
+        JunkItemNames = new List<string>();
+        ContextMenu = contextMenu;
+        AddonLifecycle = addonLc;
+        isDiscarding = false;
+        SetContexts();
+        signal = new SemaphoreSlim(0);
+
+        contextMenu.AddMenuItem(ContextMenuType.Inventory, new MenuItem()
+        {
+            Name = "Add to junk",
+            Priority = -2,
+            OnClicked = (MenuItemClickedArgs a) =>
+            {
+                if (a.Target is MenuTargetInventory item && item.TargetItem != null)
+                {
+                    var itemId = item.TargetItem.Value.ItemId;
+                    var container = (ushort)item.TargetItem.Value.ContainerType;
+                    AddItemToJunk(itemId, container);
+                }
+                return;
+            }
+        }); ;
+
+        contextMenu.AddMenuItem(ContextMenuType.Inventory, new MenuItem()
+        {
+            Name = "View Junkpile",
+            Priority= -1,
+            IsSubmenu = true,
+            OnClicked = (MenuItemClickedArgs a) =>
+            {
+                ToggleMainUI();
+                return;
+            }
+        });
+
+        contextMenu.AddMenuItem(ContextMenuType.Inventory, new MenuItem()
+        {
+            Name = "Discard junkpile",
+            OnClicked = (MenuItemClickedArgs a) =>
+            {
+                DiscardItems();
+                return;
+            }
+        });
+
 
         Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
         Configuration.Initialize(PluginInterface);
-
-        // you might normally want to embed resources and load them from the manifest stream
-        var file = new FileInfo(Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png"));
-
-        // ITextureProvider takes care of the image caching and dispose
-        var goatImage = textureProvider.GetTextureFromFile(file);
+        AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "SelectYesno", SelectYesNoSetup);
+        AddonLifecycle.RegisterListener(AddonEvent.PostReceiveEvent, "SelectYesno", UpdateDiscardStatus);
 
         ConfigWindow = new ConfigWindow(this);
-        MainWindow = new MainWindow(this, goatImage);
+        MainWindow = new MainWindow(this, JunkItemNames);
 
         WindowSystem.AddWindow(ConfigWindow);
         WindowSystem.AddWindow(MainWindow);
 
         CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
         {
-            HelpMessage = "A useful message to display in /xlhelp"
+            HelpMessage = "Opens junk window"
         });
 
         PluginInterface.UiBuilder.Draw += DrawUI;
@@ -61,7 +165,7 @@ public sealed class Plugin : IDalamudPlugin
     public void Dispose()
     {
         WindowSystem.RemoveAllWindows();
-
+        signal.Dispose();
         ConfigWindow.Dispose();
         MainWindow.Dispose();
 
@@ -72,10 +176,76 @@ public sealed class Plugin : IDalamudPlugin
     {
         // in response to the slash command, just toggle the display status of our main ui
         ToggleMainUI();
+
+    }
+
+    private unsafe void AddItemToJunk(ulong itemId, ushort container)
+    {
+        bool hq = itemId > 1000000;
+        if (hq) itemId = itemId - 1000000;
+        if (!InventoryContainerArray.Contains(container)) return;
+        var manager = InventoryManager.Instance();
+        var inventory = *manager->GetInventoryContainer((InventoryType)container);
+        SeString? itemStringInfo = "";
+
+        for (var i = 0; i < inventory.Size; i++)
+        {
+            if (inventory.Items[i].ItemID == itemId)
+            {
+                var itemInfo = *inventory.GetInventorySlot(inventory.Items[i].Slot);
+                if(!Junkpile.Any(x => x.ItemID == itemInfo.ItemID))
+                {
+                    Junkpile.Add(itemInfo);
+                    SeStringBuilder sb = new SeStringBuilder();
+                    sb.AddItemLink((uint)itemId, hq);
+                    JunkItemNames.Add(sb.ToString());
+                    itemStringInfo = sb.Append(" added to junkpile.").BuiltString;
+
+                    var response = new XivChatEntry()
+                    {
+                        Message = itemStringInfo,
+                        Type = XivChatType.Debug
+
+                    };
+                    Chat.Print(response);
+                }
+            }
+            continue;
+        }
+    }
+    public async void DiscardItems()
+    {
+        isDiscarding = true;
+        var items = Junkpile;
+        //var invContext = AgentInventoryContext.Instance();
+        //var manager = InventoryManager.Instance();
+        foreach (var item in items)
+        {
+            Discard(item);
+            await signal.WaitAsync();
+        }
+        isDiscarding = false;
+        Junkpile.Clear();
+        JunkItemNames.Clear();
+    }
+
+    private unsafe void Discard(InventoryItem item)
+    {
+        var inventoryType = (InventoryType)item.LinkedInventoryType;
+        var inventory = *inventoryManager->GetInventoryContainer(item.Container);
+        var itemInfo = inventory.GetInventorySlot(item.Slot);
+        agentInventoryContext->DiscardItem(itemInfo, itemInfo->Container, itemInfo->Slot, agentInventoryContext->OwnerAddonId);
     }
 
     private void DrawUI() => WindowSystem.Draw();
 
     public void ToggleConfigUI() => ConfigWindow.Toggle();
     public void ToggleMainUI() => MainWindow.Toggle();
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x10)]
+    private struct AddonSelectYesNoOnSetupData
+    {
+        [FieldOffset(0x8)]
+        public IntPtr TextPtr;
+    }
 }
--- SamplePlugin/Windows/MainWindow.cs
+++ SamplePlugin/Windows/MainWindow.cs
@@ -1,7 +1,7 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
-using Dalamud.Interface.Internal;
-using Dalamud.Interface.Utility;
 using Dalamud.Interface.Windowing;
 using ImGuiNET;
 
@@ -9,14 +9,14 @@ namespace SamplePlugin.Windows;
 
 public class MainWindow : Window, IDisposable
 {
-    private IDalamudTextureWrap? GoatImage;
-    private Plugin Plugin;
+    private Plugin plugin;
+    private readonly List<string> junkItems;
 
     // We give this window a hidden ID using ##
     // So that the user will see "My Amazing Window" as window title,
     // but for ImGui the ID is "My Amazing Window##With a hidden ID"
-    public MainWindow(Plugin plugin, IDalamudTextureWrap? goatImage)
-        : base("My Amazing Window##With a hidden ID", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
+    public MainWindow(Plugin plugin, List<string> itemsToDiscard)
+        : base("Junkpile", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
     {
         SizeConstraints = new WindowSizeConstraints
         {
@@ -24,33 +24,25 @@ public class MainWindow : Window, IDisposable
             MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
         };
 
-        GoatImage = goatImage;
-        Plugin = plugin;
+        this.plugin = plugin;
+        junkItems = itemsToDiscard;
     }
 
     public void Dispose() { }
 
     public override void Draw()
     {
-        ImGui.Text($"The random config bool is {Plugin.Configuration.SomePropertyToBeSavedAndWithADefault}");
-
-        if (ImGui.Button("Show Settings"))
+        if (junkItems.Any())
         {
-            Plugin.ToggleConfigUI();
+            junkItems.ForEach(item =>
+            {
+                ImGui.Text(item);
+            });
         }
 
-        ImGui.Spacing();
-
-        ImGui.Text("Have a goat:");
-        if (GoatImage != null)
+        if (ImGui.Button("Discard Junk Items"))
         {
-            ImGuiHelpers.ScaledIndent(55f);
-            ImGui.Image(GoatImage.ImGuiHandle, new Vector2(GoatImage.Width, GoatImage.Height));
-            ImGuiHelpers.ScaledIndent(-55f);
-        }
-        else
-        {
-            ImGui.Text("Image not found.");
+            plugin.DiscardItems();
         }
     }
 }
